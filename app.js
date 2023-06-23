f0fb9e63edc3b632155754749978166544e5f6f7
vany0103 <75314551+vany0103@users.noreply.github.com>
2023-06-23 11:07:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const {PORT = 3000 } = process.env
 app.get('/', (req, res) => {
-  res.send('thanks For ur time!')
+  res.send('thanking you all, For ur time!')
 })
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
